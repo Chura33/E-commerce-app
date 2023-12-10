8d020cc4f3b0ed68f6825722aafa00db388f70cc
Hilly-man <hilkiahe@gmail.com>
2023-12-10 01:09:17 +0100

    update on the landing page and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Routes } from "react-router-dom";
 
 import NavBar from "./components/general/NavBar";
+
+import Background from './components/landing/background';
+
+// Landing page components
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <h1>E-Commerce Web App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route exact path="/" components={Background} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/landing/background.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const background = () => {
+    return (
+        <section class="landing">
+            <div class="dark-overlay">
+                <div class="landing-inner">
+                    <h1 class="x-large">Fast Delivery</h1>
+                    <p class="lead">Shop and smile</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default background;
